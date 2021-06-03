ad9e890e9f5f6db4a457d526c1a8614b6ef39f09
kolaisaac <blestisaac10@gmail.com>
2021-06-03 17:42:34 +0100

    frist X-Path added

--- automation.py
+++ automation.py
@@ -6,3 +6,5 @@ driver.get("https://forms.abujaelectricity.com/DATP/")
 
 time.sleep(3)
 
+first_click = web.find_element_by_xpath('//*[@id="SelectId_0"]/div[2]/div[2]/div/span')
+first_click.click()
